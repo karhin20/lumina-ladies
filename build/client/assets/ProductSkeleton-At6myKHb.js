61de930fd8ad15e38059efa057a66feb9bea518e
karhin20 <kelvinarhin25@gmail.com>
2026-01-17 13:05:49 +0000

    seo compliant andnew add to cart for mobile

--- build/client/assets/ProductSkeleton-At6myKHb.js
+++
@@ -1,6 +0,0 @@
-import{c as s}from"./createLucideIcon-BDHzEgzW.js";import{n as e}from"./chunk-EPOLDU6W-CYlkBSH-.js";/**
- * @license lucide-react v0.462.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const r=s("ChevronLeft",[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]]),d=()=>e.jsxs("div",{className:"w-full animate-pulse",children:[e.jsx("div",{className:"relative aspect-square rounded-sm bg-muted mb-3 overflow-hidden",children:e.jsx("div",{className:"absolute inset-0 bg-gradient-to-r from-transparent via-foreground/5 to-transparent -translate-x-full animate-[shimmer_2s_infinite]"})}),e.jsxs("div",{className:"space-y-2",children:[e.jsx("div",{className:"h-4 bg-muted rounded w-3/4"}),e.jsxs("div",{className:"flex justify-between items-center",children:[e.jsx("div",{className:"h-4 bg-muted rounded w-1/4"}),e.jsx("div",{className:"h-4 bg-muted rounded w-1/4"})]}),e.jsx("div",{className:"h-3 bg-muted rounded w-1/2"})]})]});export{r as C,d as P};
